35d081a1d9fcc859ffc9628a8bbe58a9d28b1f8a
sandy <sandrap.mejia@udea.edu.co>
2021-04-07 16:30:52 -0500

    Completar el tour of heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    // tslint:disable-next-line: deprecation
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      // tslint:disable-next-line: deprecation
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    // tslint:disable-next-line: deprecation
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
+}
