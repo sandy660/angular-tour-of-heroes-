228031cbb413c528415953e0c700941e456ec5a8
sandy <sandrap.mejia@udea.edu.co>
2021-04-17 21:53:01 -0500

    se cambio del simulador al api propio

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,12 +6,12 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { MessageService } from './message.service';
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  private heroesUrl = 'api/heroes';  // URL to web api
+  private heroesUrl = environment.server_api_tour_heroes + '/heroes';
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  server_api_tour_heroes: 'http://localhost:8080/tour-api'
 };
 
 /*
